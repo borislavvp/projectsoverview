d7350e4446d3f8544d3422b2d030a644f2cf7232
Borislav Pavlov <416657@student.fontys.nl>
2021-07-07 14:10:26 +0300

    change

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@
     setup() {
       const pdfManager = withPdfManager();
       provide(PdfManagerInjectionKey, pdfManager);
+
       onMounted(() => {
         pdfManager.prepareDocuments().catch((e) => console.log(e));
       });
