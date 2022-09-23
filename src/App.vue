0c0451c9bc7c2e9c6e47f7efedb12df2f8e1a9d8
Borislav Pavlov <416657@student.fontys.nl>
2022-09-23 20:00:59 +0200

    updated

--- src/App.vue
+++ src/App.vue
@@ -13,51 +13,50 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, provide } from "@vue/composition-api";
-  import HeaderBar from "./components/HeaderBar.vue";
-  import DocumentsList from "./components/DocumentsList.vue";
-  import { withPdfManager } from "./providers/withPdfManager";
-  import { PdfManagerInjectionKey } from "./providers/types/PdfManagerInjectionKey";
-  export default defineComponent({
-    components: {
-      HeaderBar,
-      DocumentsList,
-    },
-    setup() {
-      const pdfManager = withPdfManager();
-      provide(PdfManagerInjectionKey, pdfManager);
-
-      onMounted(() => {
-        pdfManager.prepareDocuments().catch((e) => console.log("Error:", e));
-      });
-    },
-  });
+import { defineComponent, onMounted, provide } from "@vue/composition-api";
+import HeaderBar from "./components/HeaderBar.vue";
+import DocumentsList from "./components/DocumentsList.vue";
+import { withPdfManager } from "./providers/withPdfManager";
+import { PdfManagerInjectionKey } from "./providers/types/PdfManagerInjectionKey";
+export default defineComponent({
+  components: {
+    HeaderBar,
+    DocumentsList,
+  },
+  setup() {
+    const pdfManager = withPdfManager();
+    provide(PdfManagerInjectionKey, pdfManager);
+    onMounted(() => {
+      pdfManager.prepareDocuments().catch((e) => console.log("Error:", e));
+    });
+  },
+});
 </script>
 <style>
-  @media (max-width: 481px) {
-    ::-webkit-scrollbar {
-      display: none;
-    }
-  }
-  ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(221, 221, 221, 0.3);
-    box-shadow: inset 0 0 6px rgba(209, 209, 209, 0.3);
-    background-color: #ffffff;
-  }
-
+@media (max-width: 481px) {
   ::-webkit-scrollbar {
-    width: 12px;
-    background-color: #f5f5f5;
+    display: none;
   }
+}
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(221, 221, 221, 0.3);
+  box-shadow: inset 0 0 6px rgba(209, 209, 209, 0.3);
+  background-color: #ffffff;
+}
 
-  ::-webkit-scrollbar-thumb {
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    background-color: rgb(168, 168, 168);
-  }
+::-webkit-scrollbar {
+  width: 12px;
+  background-color: #f5f5f5;
+}
 
-  /* Handle on hover */
-  ::-webkit-scrollbar-thumb:hover {
-    background: rgb(150, 150, 150);
-  }
+::-webkit-scrollbar-thumb {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: rgb(168, 168, 168);
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(150, 150, 150);
+}
 </style>
