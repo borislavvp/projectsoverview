9743ecf54872f8509135deb1df319ddb1be4c314
Borislav Pavlov <416657@student.fontys.nl>
2022-10-01 12:56:10 +0200

    pwa options

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  pwa: {
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black-transculent",
+    themeColor: "#FFFFFF",
+  },
+};
